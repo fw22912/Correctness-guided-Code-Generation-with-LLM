53533cec59de9b6ec053135481771e9c91b36ad2
Lee <fw22912@bristol.ac.uk>
2024-07-01 19:09:46 +0100

    Stores the generated function into a new C file

--- CorrectnessProof/Gemini.py
+++ CorrectnessProof/Gemini.py
@@ -3,18 +3,38 @@ import os
 import argparse
 import Prompt_Generator as pg
 
-#Generate a prompt
-parser = argparse.ArgumentParser()
-parser.add_argument("file_path", type=str)
-args = parser.parse_args()
+def configure_genai(api_key):
+    genai.configure(api_key=api_key)
 
-prompt = pg.main(args.file_path)
+def generate_prompt(file_path):
+    return pg.main(file_path)
 
+def generate_content(prompt):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content(prompt)
+    return response.text
 
-#Linking Gemini
-genai.configure(api_key=os.environ['GENAI_API_KEY'])
+def clean_code(response_text):
+    cleaned_text = response_text.replace('```c', '').replace('```', '').strip()
+    return cleaned_text
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+def generate_file(cleaned_text, original_file_path):
+    file_name = os.path.splitext(original_file_path)[0]
+    harness_file_name = os.path.join("/Users/hyoyeon/Desktop/UNI/Faculty Internship/Correctness-guided-Code-Generation-with-LLM/CorrectnessProof/Proof_Harness", file_name + "_harness.c")
 
-response = model.generate_content(prompt)
-print(response.text)
+    with open(harness_file_name, 'w') as f:
+        f.write(cleaned_text)
+
+def main(file_path):
+    configure_genai(api_key=os.environ['GENAI_API_KEY'])
+    prompt = generate_prompt(file_path)
+    response_text = generate_content(prompt)
+    print(response_text)
+    cleaned_text = clean_code(response_text)
+    generate_file(cleaned_text, file_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", type=str)
+    args = parser.parse_args()
+    main(args.file_path)
---
+++ CorrectnessProof/traversal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void inOrderTraversal(struct node *node){
+    if (node == NULL) return;
+
+    inOrderTraversal(node->leftNode);
+    printf("\t%d\t", node->data);
+    inOrderTraversal(node->rightNode);
+}
+
+void preOrderTraversal(struct node *node){
+    if (node == NULL) return;
+
+    printf("\t%d\t", node->data);
+    preOrderTraversal(node->leftNode);
+    preOrderTraversal(node->rightNode);
+}
+
+
+void postOrderTraversal(struct node *node){
+    if (node == NULL) return;
+    postOrderTraversal(node->leftNode);
+    postOrderTraversal(node->rightNode);
+    printf("\t%d\t", node->data);
+}
+
+int main(void){
+    return 0;
+}
